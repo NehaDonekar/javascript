71342c8407e6e37aceee89e3c576a2ccc85d5d9b
Neha Donekar <113767279+NehaDonekar@users.noreply.github.com>
2024-01-27 23:37:42 +0530

    arrow function

---
+++ arrow.js
@@ -0,0 +1,11 @@
+
+
+// const addTwo =(num1,num2) => {
+//     return num1 + num2
+// }
+// console.log(addTwo(3,4))
+
+// const addTwo =(num1,num2) =>  num1 + num2
+
+const addTwo =(num1,num2) =>  (num1 + num2)
+console.log(addTwo(3,4))
