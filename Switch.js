8ccb3277b8172ddb2374bd84a64acccc8278fd4f
Neha Donekar <113767279+NehaDonekar@users.noreply.github.com>
2024-01-28 23:40:45 +0530

    Switch control flow

---
+++ Switch.js
@@ -0,0 +1,13 @@
+let rating = 3
+
+switch(rating)
+{
+    case 1:
+    case 2:
+    case 3:
+    case 4:            
+        console.log("Sorry Sir/Mam for your wealingness ,thank you for visit . message from customer care team")
+        break;
+    case 5:
+        console.log("thank you for visit . message from customer care team")    
+}
